7eb80be5e807d6f5067b89d8b03eaddcb813559f
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-02-20 23:42:24 +0300

    modify workflow

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.function.Function;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello spring ci/cd";
+    }
+    
+}
