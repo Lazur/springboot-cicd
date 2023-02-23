9df08d09c0ed1e024d82745869b27afc0a4d1e58
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-02-23 11:35:54 +0300

    new way

--- .github/workflows/kill-app.sh
+++ .github/workflows/kill-app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Get the PID of the "myapp" process
-PID=$(pgrep -f 'java -jar demo-0.0.1-SNAPSHOT.jar')
+PID=$(docker ps -q)
 
 # If the process is running, terminate it
 if [ -n "$PID" ]; then
   echo "Terminating spring app process (PID: $PID)"
-  kill $PID
+  docker kill $PID
 else
   echo "spring app process is not running"
 fi
